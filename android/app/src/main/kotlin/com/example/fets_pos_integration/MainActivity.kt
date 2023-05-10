38fb1358543620a95ad09542f0466152183b38f0
ani <godwin@istrategytech.com>
2023-05-10 16:25:48 +0100

    wip: fets integration

--- android/app/src/main/kotlin/com/example/fets_pos_integration/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/fets_pos_integration/MainActivity.kt
@@ -1,6 +1,38 @@
 package com.example.fets_pos_integration
 
+import android.app.Activity
+import androidx.activity.ComponentActivity
+import androidx.activity.result.ActivityResultCallback
 import io.flutter.embedding.android.FlutterActivity
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val FETS_CHANNEL = "com.irecharge.fets";
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, FETS_CHANNEL).setMethodCallHandler{
+            call, result ->
+
+        }
+    }
+
+
+    fun makePayment(){
+        val startActivityForResult =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { activityResult ->
+                val result = activityResult?.data
+
+
+            }
+    }
+
+        private fun <I, O> Activity.registerForActivityResult(
+        contract: ActivityResultContract<I, O>,
+        callback: ActivityResultCallback<O>
+    ) = (this as ComponentActivity).registerForActivityResult(contract, callback)
+
 }
