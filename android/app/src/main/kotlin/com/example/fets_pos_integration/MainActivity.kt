485f3aa884af72433d75a6aeee2c3da5cb964e44
collins ihezie <collinsihezie6@gmail.com>
2023-05-10 20:13:02 +0100

    setup method channel

--- android/app/src/main/kotlin/com/example/fets_pos_integration/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/fets_pos_integration/MainActivity.kt
@@ -7,44 +7,56 @@ import androidx.activity.result.ActivityResultCallback
 import io.flutter.embedding.android.FlutterActivity
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.os.bundleOf
 import com.example.fets_pos_integration.models.FetsTransactionModel
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
-    private val FETS_CHANNEL = "com.irecharge.fets";
-    private val FETS_INTENT = "fets.pos.checkout"
+    companion object{
+        private const val FETS_CHANNEL = "com.irecharge.fets";
+        private const val FETS_INTENT = "fets.pos.checkout";
+    }
 
+    lateinit var result: MethodChannel.Result
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, FETS_CHANNEL).setMethodCallHandler{
-            call, result ->
+                call, result ->
+            this.result = result;
 
+            when(call.method) {
+                "cardPayment" -> {
+                    if(call.argument<String>("amount") != null){
+                        makePayment(FetsTransactionModel(amount = call.argument<String>("amount")!!, reference = call.argument<String?>("reference")))
+                    }
+                }
+            }
         }
     }
 
 
-    fun makePayment(){
-        val startActivityForResult =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { activityResult ->
-                val result = activityResult?.data
+    private fun makePayment(paymentModel: FetsTransactionModel){
+        val startActivityForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { activityResult ->
+            val response = activityResult?.data
 
+            //TODO parse result as payment response
+            result.success(response?.data)
+        }
 
-            }
+//        val paymentExtra = FetsTransactionModel(amount = "100.00")
+//
+//
+//        val intent = Intent("fets.pos.checkout")
+//            .putExtra("amount", "100.00")
 
-        val intent = Intent(FETS_INTENT)
-
-        val paymentExtra = FetsTransactionModel(
-            amount = "100.00")
 
         //TODO: parse json and add extra to the intent
-        startActivityForResult.launch(intent)
+        startActivityForResult.launch(Intent(FETS_INTENT)
+            //TODO add extra key
+            .putExtra("", bundleOf(Pair("amount", paymentModel.amount), Pair("reference", paymentModel.reference))))
     }
 
-        private fun <I, O> Activity.registerForActivityResult(
-        contract: ActivityResultContract<I, O>,
-        callback: ActivityResultCallback<O>
-    ) = (this as ComponentActivity).registerForActivityResult(contract, callback)
-
+    private fun <I, O> Activity.registerForActivityResult(contract: ActivityResultContract<I, O>, callback: ActivityResultCallback<O>) = (this as ComponentActivity).registerForActivityResult(contract, callback)
 }
