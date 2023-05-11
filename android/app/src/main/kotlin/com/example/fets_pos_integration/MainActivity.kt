d30b6560d957d9e006f095b785fdb8b3e6c95b3e
ani <godwin@istrategytech.com>
2023-05-11 15:28:32 +0100

    refactor intent request

--- android/app/src/main/kotlin/com/example/fets_pos_integration/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/fets_pos_integration/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.fets_pos_integration
 
 import android.app.Activity
+import android.content.ComponentName
 import android.content.Intent
 import androidx.activity.ComponentActivity
 import androidx.activity.result.ActivityResultCallback
@@ -14,8 +15,10 @@ import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
     companion object{
-        private const val FETS_CHANNEL = "com.irecharge.fets";
-        private const val FETS_INTENT = "fets.pos.checkout";
+        private const val FETS_CHANNEL = "com.irecharge.fets"
+        private const val FETS_INTENT = "fets.pos.checkout"
+        private const val IRECHARGE_PACKAGE_NAME = "com.fets.infostrategypayment"
+        private const val FETS_PACKAGE_NAME = "com.fets.infostrategypayment.Purchase"
     }
 
     lateinit var result: MethodChannel.Result
@@ -29,7 +32,7 @@ class MainActivity: FlutterActivity() {
             when(call.method) {
                 "cardPayment" -> {
                     if(call.argument<String>("amount") != null){
-                        makePayment(FetsTransactionModel(amount = call.argument<String>("amount")!!, reference = call.argument<String?>("reference")))
+                        makePayment(FetsTransactionModel(amount = call.argument<String>("amount")!!, reference = call.argument<String?>("reference")!!))
                     }
                 }
             }
@@ -45,17 +48,13 @@ class MainActivity: FlutterActivity() {
             result.success(response?.data)
         }
 
-//        val paymentExtra = FetsTransactionModel(amount = "100.00")
-//
-//
-//        val intent = Intent("fets.pos.checkout")
-//            .putExtra("amount", "100.00")
-
-
-        //TODO: parse json and add extra to the intent
         startActivityForResult.launch(Intent(FETS_INTENT)
-            //TODO add extra key
-            .putExtra("", bundleOf(Pair("amount", paymentModel.amount), Pair("reference", paymentModel.reference))))
+            .putExtra("data", bundleOf(Pair("amount", paymentModel.amount), Pair("reference", paymentModel.reference)))
+            .setAction(
+                FETS_INTENT)
+            .setPackage(IRECHARGE_PACKAGE_NAME)
+            .addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES)
+            .setComponent(ComponentName(IRECHARGE_PACKAGE_NAME, FETS_PACKAGE_NAME)))
     }
 
     private fun <I, O> Activity.registerForActivityResult(contract: ActivityResultContract<I, O>, callback: ActivityResultCallback<O>) = (this as ComponentActivity).registerForActivityResult(contract, callback)
