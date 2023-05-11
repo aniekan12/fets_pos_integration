9f1c88acc58e74bb18347dd00fd416b7cbf0e13a
collins ihezie <collinsihezie6@gmail.com>
2023-05-11 10:28:43 +0100

    added FetsCheckoutBroadcastReceiver class

--- android/app/src/main/kotlin/com/example/fets_pos_integration/models/FetsTransactionModel.kt
+++ android/app/src/main/kotlin/com/example/fets_pos_integration/models/FetsTransactionModel.kt
@@ -5,5 +5,5 @@ import androidx.annotation.Keep
 @Keep
 data class FetsTransactionModel(
     val amount : String,
-    val reference : String = ""
+    val reference : String?
 )
---
+++ android/app/src/main/kotlin/com/example/fets_pos_integration/receivers/FetsCheckoutBroadcastReceiver.kt
@@ -0,0 +1,12 @@
+package com.example.fets_pos_integration.receivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+
+class FetsCheckoutBroadcastReceiver : BroadcastReceiver() {
+    override fun onReceive(ctx: Context?, intent: Intent) {
+        //Todo get fets response data
+    }
+}
